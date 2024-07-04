23d7d93e4f74f48a8e568afd01e4190e0ad9d82e
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-07-04 20:20:38 +0100

    Update cockpit.py

--- src/states/cockpit.py
+++ src/states/cockpit.py
@@ -25,8 +25,15 @@ class CockPit:
         self.engine = engine
         cockpit_image = import_image("assets/cockpit.png")
 
-        self.colors = [(26, 12, 49), (53, 54, 91), (104, 107, 114)]  # wall tile colors
-        self.layers = [self._surface_mask_color(cockpit_image, i) for i in self.colors]
+        self.cockpit_image = pg.image.load("assets/cockpit.png").convert_alpha()
+
+        self.particle_colors = [(26, 12, 49), (53, 54, 91), (104, 107, 114)]
+        color_sets = [
+            [(26, 12, 49), (53, 54, 91), (104, 107, 114), (136, 151, 185), (195, 205, 220), (255, 255, 255)]
+        ]
+        self.layers = [
+            self._shift_colors(surface=self.cockpit_image, color_sets=color_sets, n=2 - i) for i in range(3)
+        ]
 
         self.particles: list[Particle] = []
         self.particle_timer = 0.0
@@ -34,13 +41,13 @@ class CockPit:
         self.player = Player(engine)
 
     @staticmethod
-    def _surface_mask_color(
-        surface: pygame.Surface, color: tuple[int, int, int]
-    ) -> pygame.Surface:
-        surface.set_colorkey(color)
-        array = pygame.surfarray.array_colorkey(surface)
-        surface.set_colorkey(None)
-        return pygame.surfarray.make_surface(array).convert()
+    def _shift_colors(surface: pg.Surface, color_sets: list[list[tuple[int, int, int]]], n: int) -> pg.Surface:
+        surface = surface.copy()
+        array = pg.PixelArray(surface)
+        for colors in color_sets:
+            for old_color, new_color in zip(colors, [(0, 0, 0)] * n + colors):
+                array.replace(old_color, new_color)
+        return surface
 
     def handle_events(self, event):
         if event.type == pg.KEYDOWN:
@@ -51,45 +58,52 @@ class CockPit:
 
     def render(self):
         self.engine.screen.fill("black")
-        # self._render_particles()
 
-        # FIXME: Inefficient Surface Instantiation
+        radius = 74
         for n, layer in enumerate(self.layers):
-            radius = 64 - 16 * (2 - n)
-            for color in self.colors[0 : n + 1]:
-                img = layer.copy()
-                img.fill(color, special_flags=pg.BLEND_RGB_ADD)
-                circle_surf = pg.Surface((radius * 2, radius * 2))
-                circle_surf.fill("white")
-                pg.draw.circle(circle_surf, "black", (radius, radius), radius)
-                circle_surf.blit(
-                    img,
-                    (
-                        -(self.player.rect.centerx - radius - 32),
-                        -(self.player.rect.centery - radius - 27),
-                    ),
-                    special_flags=pg.BLEND_RGB_MAX,
-                )
+            surf = pg.Surface(layer.size)
+            surf.fill("white")
+            pg.draw.circle(
+                surf,
+                "black",
+                (int(self.player.rect.centerx) - 32,
+                 int(self.player.rect.centery) - 27),
+                radius
+            )
+            self._render_particles(surf, self.particle_colors[n])
+            surf.blit(layer, (0, 0), special_flags=pg.BLEND_RGB_ADD)
+            surf.set_colorkey("white")
+            self.engine.screen.blit(surf, (32, 27))
+            radius -= 20
 
-                circle_surf.set_colorkey("white", pg.RLEACCEL)
-                pos = pg.Vector2(self.player.rect.center).elementwise() - radius
-                self.engine.screen.blit(circle_surf, pos)
-                radius -= 16
+        self._move_particles()
 
         self.player.update()
 
-    def _render_particles(self):
+    def _render_particles(self, surface, color):
+        for particle in self.particles:
+            if (
+                    self.cockpit_image.get_at((particle.x, particle.y)) == (0, 0, 0, 0)
+                    and surface.get_at((particle.x, particle.y)) == (0, 0, 0, 255)
+            ):
+                surface.set_at((particle.x, particle.y), color)
+
+    def _move_particles(self):
         for particle in self.particles.copy():
-            if particle.x > 110:
+            if particle.x > 80:
                 self.particles.remove(particle)
                 continue
 
             particle.x += particle.vel * self.engine.dt
-            self.engine.screen.set_at((particle.x, particle.y), 0x353658)
 
         self.particle_timer += self.engine.dt
         if self.particle_timer > 0.5:
             self.particle_timer = 0
+
             self.particles.append(
-                Particle(x=27, y=random.randint(45, 85), vel=random.randint(100, 200))
+                Particle(
+                    x=0,
+                    y=random.randint(13, 53),
+                    vel=random.randint(100, 200)
+                )
             )
