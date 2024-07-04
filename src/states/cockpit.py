c356175ec1294813d1eefb633df9585a6b99db35
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-07-04 20:10:08 +0100

    Update

--- src/states/cockpit.py
+++ src/states/cockpit.py
@@ -20,62 +20,72 @@ class CockPit:
 
         self.cockpit_image = pg.image.load("assets/cockpit.png").convert_alpha()
 
-        self.colors = [(26, 12, 49), (53, 54, 91), (104, 107, 114)]
-        self.layers = [self._shift_colors(surface=self.cockpit_image, colors=self.colors, n=2-i) for i in range(3)]
+        self.particle_colors = [(26, 12, 49), (53, 54, 91), (104, 107, 114)]
+        color_sets = [
+            [(26, 12, 49), (53, 54, 91), (104, 107, 114), (136, 151, 185), (195, 205, 220), (255, 255, 255)]
+        ]
+        self.layers = [
+            self._shift_colors(surface=self.cockpit_image, color_sets=color_sets, n=2 - i) for i in range(3)
+        ]
 
         self.particles: list[Particle] = []
         self.particle_timer = 0.0
 
     @staticmethod
-    def _shift_colors(surface: pg.Surface, colors: list[tuple[int, int, int]], n: int) -> pg.Surface:
+    def _shift_colors(surface: pg.Surface, color_sets: list[list[tuple[int, int, int]]], n: int) -> pg.Surface:
         surface = surface.copy()
         array = pg.PixelArray(surface)
-        for old_color, new_color in zip(colors, [(0, 0, 0)] * n + colors):
-            array.replace(old_color, new_color)
+        for colors in color_sets:
+            for old_color, new_color in zip(colors, [(0, 0, 0)] * n + colors):
+                array.replace(old_color, new_color)
         return surface
-
-    # @staticmethod
-    # def _surface_mask_color(surface: pygame.Surface, color: tuple[int, int, int]) -> pygame.Surface:
-    #     surface.set_colorkey(color)
-    #     array = pygame.surfarray.array_colorkey(surface)
-    #     surface.set_colorkey(None)
-    #     return pygame.surfarray.make_surface(array).convert()
 
     def render(self):
         self.engine.screen.fill("black")
 
-        radius = 64
-        for layer in self.layers:
+        radius = 74
+        for n, layer in enumerate(self.layers):
             surf = pg.Surface(layer.size)
-            surf.fill("black")
+            surf.fill("white")
             pg.draw.circle(
                 surf,
-                "white",
+                "black",
                 (int(self.engine.player.rect.centerx) - 32,
                  int(self.engine.player.rect.centery) - 27),
                 radius
             )
-            surf.blit(layer, (0, 0), special_flags=pg.BLEND_RGB_MULT)
-            surf.set_colorkey("black")
+            self._render_particles(surf, self.particle_colors[n])
+            surf.blit(layer, (0, 0), special_flags=pg.BLEND_RGB_ADD)
+            surf.set_colorkey("white")
             self.engine.screen.blit(surf, (32, 27))
-            radius -= 16
+            radius -= 20
 
-    def _render_particles(self):
+        self._move_particles()
+
+    def _render_particles(self, surface, color):
+        for particle in self.particles:
+            if (
+                    self.cockpit_image.get_at((particle.x, particle.y)) == (0, 0, 0, 0)
+                    and surface.get_at((particle.x, particle.y)) == (0, 0, 0, 255)
+            ):
+                surface.set_at((particle.x, particle.y), color)
+
+    def _move_particles(self):
         for particle in self.particles.copy():
-            if particle.x > 110:
+            if particle.x > 80:
                 self.particles.remove(particle)
                 continue
 
             particle.x += particle.vel * self.engine.dt
-            self.engine.screen.set_at((particle.x, particle.y), 0x353658)
 
         self.particle_timer += self.engine.dt
         if self.particle_timer > 0.5:
             self.particle_timer = 0
+
             self.particles.append(
                 Particle(
-                    x=27,
-                    y=random.randint(45, 85),
+                    x=0,
+                    y=random.randint(13, 53),
                     vel=random.randint(100, 200)
                 )
             )
